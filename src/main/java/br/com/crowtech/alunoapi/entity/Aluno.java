089eec8906ae6d6b48916f148c6324a2eb105636
Thiago Oliveira <thiagop_o@hotmail.com>
2021-07-26 20:00:05 -0300

    Colocado os construtores nas classes aluno e materia

--- src/main/java/br/com/crowtech/alunoapi/entity/Aluno.java
+++ src/main/java/br/com/crowtech/alunoapi/entity/Aluno.java
@@ -1,11 +1,13 @@
 package br.com.crowtech.alunoapi.entity;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
 public class Aluno {
     private int id;
     private String nome;
@@ -13,4 +15,12 @@ public class Aluno {
     private String rA;
     private Date dataDeNascimento;
     private List<Materia> materias;
+
+    public Aluno(String nome, String cpf, String rA, Date dataDeNascimento, List<Materia> materias) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.rA = rA;
+        this.dataDeNascimento = dataDeNascimento;
+        this.materias = materias;
+    }
 }
--- src/main/java/br/com/crowtech/alunoapi/entity/Materia.java
+++ src/main/java/br/com/crowtech/alunoapi/entity/Materia.java
@@ -1,8 +1,17 @@
 package br.com.crowtech.alunoapi.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
 public class Materia {
     private int id;
     private String materia;
     private String nomeProfessor;
+
+    public Materia(String materia, String nomeProfessor) {
+        this.materia = materia;
+        this.nomeProfessor = nomeProfessor;
+    }
 }
