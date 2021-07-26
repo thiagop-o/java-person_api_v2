0352fd8d2efaec71294bb39e2f2258e6a95881fb
Thiago Oliveira <thiagop_o@hotmail.com>
2021-07-26 19:44:13 -0300

    first commit

---
+++ src/main/java/br/com/crowtech/alunoapi/AlunoapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.crowtech.alunoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlunoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlunoapiApplication.class, args);
+	}
+
+}
